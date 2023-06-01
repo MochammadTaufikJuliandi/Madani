3fca29969f03d91b1bf41400544542bb74fe75f3
MochammadTaufikJuliandi <taufik.juliandi4@gmail.com>
2023-06-01 13:00:55 +0700

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,6 @@ const App = () => {
       <Header data={landingPageData.Header} />
       <Features data={landingPageData.Features} />
       <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
       <Gallery data={landingPageData.Gallery} />
       <Testimonials data={landingPageData.Testimonials} />
       <Team data={landingPageData.Team} />
